38ac9f4fa7aac42449a94b86b36121145a92597a
Bruno Henrique Rocha de Almeida <bruno.almeida@esx.com.br>
2020-01-07 15:45:49 -0300

    apiorchestration modified

--- ApiGateway.API/Application/ApiOrchestration.cs
+++ ApiGateway.API/Application/ApiOrchestration.cs
@@ -15,7 +15,7 @@ namespace ApiGateway.API
 
             var weatherApiClientConfig = weatherService.GetClientConfig();
 
-            orchestrator.AddApi("weatherservice", "http://sigma-weather/")
+            orchestrator.AddApi("weatherservice", "http://sigma-weather.sigma.local/")
                                 //Get
                                 .AddRoute("forecast", GatewayVerb.GET, new RouteInfo { Path = "weatherforecast/forecast", ResponseType = typeof(IEnumerable<WeatherForecast>) })
                                 //Get using custom HttpClient
@@ -30,7 +30,7 @@ namespace ApiGateway.API
                                 .AddRoute("update", GatewayVerb.PUT, new RouteInfo { Path = "weatherforecast/types/update", RequestType = typeof(UpdateWeatherTypeRequest), ResponseType = typeof(string[]) })
                                 //Delete
                                 .AddRoute("remove", GatewayVerb.DELETE, new RouteInfo { Path = "weatherforecast/types/remove/", ResponseType = typeof(string[]) })
-                        .AddApi("stockservice", "http://sigma-stock/")
+                        .AddApi("stockservice", "http://sigma-stock.sigma.local/")
                                 .AddRoute("stocks", GatewayVerb.GET, new RouteInfo { Path = "stock", ResponseType = typeof(IEnumerable<StockQuote>) })
                                 .AddRoute("stock", GatewayVerb.GET, new RouteInfo { Path = "stock/", ResponseType = typeof(StockQuote) });
         }
